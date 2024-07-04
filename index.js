9a3dcde7e42bf86efeda822fd26eecbffdae5223
Ivan Gutsev <igutsevmail2006@gmail.com>
2024-07-04 08:17:38 +0300

    fixed one bug where when I typed for example 1 + 2 and the result was 3 if i pressed for example 5 it would display 15

--- index.js
+++ index.js
@@ -8,7 +8,26 @@ const allClearBtn = document.querySelector('[data-all-clear]')
 let num1 = "";
 let num2 = "";
 let operator = undefined;
+let currentNum = "one";
+let displayNumber = "";
 
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        let btn = button.innerText;
+        appendNumber(btn);
+        updateDisplay(display, displayNumber);
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        let btn = button.innerText;
+        chooseOperation(btn);
+        updateDisplay(display, displayNumber);
+    })
+})
+
+// all the functions are below this line :)
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -25,12 +44,12 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operate(num1, num2, operation) {
-    let  computation;
-    const prev = parseFloat(num1);
-    const current = parseFloat(num2);
+function operate(numOne, numTwo, operator) {
+    let computation;
+    const prev = parseFloat(numOne);
+    const current = parseFloat(numTwo);
     if (isNaN(prev) || isNaN(current)) return;
-    switch (operation) {
+    switch (operator) {
         case '+':
             computation = add(prev, current);
             break;
@@ -46,9 +65,15 @@ function operate(num1, num2, operation) {
         default:
             return;
         }
-        num1 = computation;
-        operation = undefined;
+        num1 = computation.toString();
+        displayNumber = num1;
         num2 = '';
+        currentNum = "one";
+        updateDisplay(display, displayNumber);
+
+        console.log(num1);
+        console.log(num2);
+        console.log(operator);
 }
 
 function updateDisplay(display, num) {
@@ -56,22 +81,31 @@ function updateDisplay(display, num) {
 }
 
 function chooseOperation(operation) {
-    console.log(num1);
-    if (num2 !== '') {
-        operate(num1, num2, operation);
-    } else {
+    if (operator && num2 !== "") {
+        console.log("will operate ");
+        operate(num1, num2, operator);
+    } 
+    else {
         operator = operation;
+        currentNum = "two";
     }
+
+    console.log(operator);
 }
 
-function appendNumber(number) {
-    if (number === '.' && display.innerText.includes('.')) return;
+function appendNumber(num) {
+    // we want to allow only one . (we dont want something like 5.55.55 because it doesnt make sense mathematically to say "5 point 55 point 55") 
+    if (num === '.' && display.innerText.includes('.')) return;
     // if currentNum = one -> append to num1
     // else to num2
     // if you still can't do it it wont hurt to ask the TOP discord for help
+
+    // decide to which number to add num
     if (currentNum === "one") {
-        num1 = num1.toString + number.toString;
+        num1 = num1.toString() + num.toString();
+        displayNumber = num1;
     } else {
-        num2 = num2.toString + number.toString;
+        num2 = num2.toString() + num.toString();
+        displayNumber = num2;
     }
 }
